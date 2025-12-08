9fb67481b51a0089df4a6c1cfe96d41ed2a37d01
Safwaan Nour <safwaan.mnour@gmail.com>
2025-12-08 02:56:49 +0000

    Update window_functions.sql

--- SQL/ queries/window_functions.sql
+++ SQL/ queries/window_functions.sql
@@ -1,6 +1,6 @@
 -- Rank transactions for each user based on amount (highest to lowest)--
 SELECT u.first_name, u.last_name, t.transaction_id, t.amount, t.transaction_date,
-       ROW_NUMBER() OVER (PARTITION BY u.user_id ORDER BY t.amount DESC) AS transaction_rank
+ROW_NUMBER() OVER (PARTITION BY u.user_id ORDER BY t.amount DESC) AS transaction_rank
 FROM transactions t
 INNER JOIN accounts AS a 
 ON t.account_id = a.account_id
